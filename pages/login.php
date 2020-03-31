d7808da0854119de8bad25d6d4e5d8ac0241d30f
twojtal <60483916+twojtal@users.noreply.github.com>
2020-03-31 17:52:52 -0400

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -2,7 +2,7 @@
 <body>
 
 <form action="" method="post">
-Username:<input type="text" name="username"><br>
+Username: <input type="text" name="username"><br>
 Password: <input type="password" name="password"><br>
 <input type="submit" name="login-user" value="Login">
 </form>
