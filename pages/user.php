ad204a730a1f1664208ad254b540a5174f254611
DeQuan <ddouglas1@pride.hofstra.edu>
2020-04-08 21:08:13 -0700

    Displays user info on user.php and has a functioning sign out button

--- pages/user.php
+++ pages/user.php
@@ -8,7 +8,14 @@ $info = getInfo($id,$pass);
 
 <html>
 <body>
-Email: <?php echo $info["email"]?>
+Email: <?php echo $info["email"]?><br> <!-- // Displays user email -->
+Name: <?php echo $info["fullname"]?><br> <!-- // Displays user name -->
+Hofstra ID: <?php echo $info["hofstraid"]?><br><br> <!-- // Displays user's hofstra ID -->
+
+<a href="sell.php" title="sellBook">Sell Book</a><br>
+<a href="myBooks.php" title="userBooks">My Books</a><br>
+<a href="search.php" title="search">Search</a><br>
+<a href="logout.php" title="logout">Sign Out</a>
 </body>
 <html>
 
